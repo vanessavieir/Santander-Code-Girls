9e40ec6a9e5eb5e7443bbbb5b1b608e947a2fe98
diegoednaldo <vanessa2209vieira@gmail.com>
2022-07-10 10:53:50 -0300

    Primeiro Projeto Java

---
+++ JAVA/src/horadosistema/HoraDoSistemaa.java
@@ -0,0 +1,13 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistemaa {
+
+	public static void main(String[] args) {
+		Date data = new Date();
+		System.out.println("A hora do sistema é:");
+		System.out.println(data.toString());
+	}
+
+}
